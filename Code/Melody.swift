33b752687740d812bb5440533672d721d41a7c76
Boris Bügling <boris@icculus.org>
2015-12-02 01:02:38 +0100

    Add some documentation

--- Code/Melody.swift
+++ Code/Melody.swift
@@ -11,18 +11,27 @@ private func makeAppleMusicUrl(url: NSURL, _ propertyName: String) -> NSURL {
 	return NSURL(string: urlString) ?? undefined("can't generate \(propertyName)")
 }
 
+/// Model object for an iTunes album
 public struct Album: Decodable {
-	let artist: Artist
-	let artworkUrl: NSURL
-	let genre: String
-	let identifier: String
-	let name: String
-	let url: NSURL
+	/// Artist who made the album
+	public let artist: Artist
+	/// URL to an image of the album art
+	public let artworkUrl: NSURL
+	/// Genre
+	public let genre: String
+	/// iTunes identifier
+	public let identifier: String
+	/// Album name
+	public let name: String
+	/// iTunes URL for the album
+	public let url: NSURL
 
-	var appleMusicUrl: NSURL {
+	/// Apple Music URL for the album
+	public var appleMusicUrl: NSURL {
 		return makeAppleMusicUrl(url, "Album.appleMusicUrl")
 	}
 
+	/// Decode album from JSON
 	public static func decode(json: AnyObject) throws -> Album {
 		let artworkUrlString: String = try json => "artworkUrl100"
 		let urlString: String = try json => "trackViewUrl"
@@ -38,16 +47,23 @@ public struct Album: Decodable {
 	}
 }
 
+/// Model object for an iTunes artist
 public struct Artist: Decodable {
-	let identifier: String
-	let imageUrl: NSURL
-	let name: String
-	let url: NSURL
+	/// iTunes identifier
+	public let identifier: String
+	/// URL to an image of the artist
+	public let imageUrl: NSURL
+	/// Artist name
+	public let name: String
+	/// iTunes URL for the artist
+	public let url: NSURL
 
-	var appleMusicUrl: NSURL {
+	/// Apple Music URL for the artist
+	public var appleMusicUrl: NSURL {
 		return makeAppleMusicUrl(url, "Artist.appleMusicUrl")
 	}
 
+	/// Decode artist from JSON
 	public static func decode(json: AnyObject) throws -> Artist {
 		let artistUrlString: String = try json => "artistUrl"
 		let imageUrlString: String = try json => "artistImage"
@@ -61,18 +77,27 @@ public struct Artist: Decodable {
 	}
 }
 
+/// Model object for an iTunes track
 public struct Track: Decodable {
-	let artist: Artist
-	let artworkUrl: NSURL
-	let genre: String
-	let identifier: String
-	let name: String
-	let url: NSURL
+	/// Artist who made the track
+	public let artist: Artist
+	/// URL to an image of the album art
+	public let artworkUrl: NSURL
+	/// Genre
+	public let genre: String
+	/// iTunes identifier
+	public let identifier: String
+	/// Track name
+	public let name: String
+	/// iTunes URL of the track
+	public let url: NSURL
 
-	var appleMusicUrl: NSURL {
+	/// Apple Music URL of the track
+	public var appleMusicUrl: NSURL {
 		return makeAppleMusicUrl(url, "Track.appleMusicUrl")
 	}
 
+	/// Decode track from JSON
 	public static func decode(json: AnyObject) throws -> Track {
 		//let artworkUrlString: String = try json => "artwork" => "url"
 		let artworkUrlString: String = try json => "artworkUrl100"
@@ -94,6 +119,7 @@ enum Entity: String {
 	case Track = "track"
 }
 
+/// Search the iTunes Store for albums and tracks
 public class Melody {
 	private let baseUrl = "https://sticky-summer-lb.inkstone-clients.net/api/v1/searchMusic"
 	private let session: NSURLSession
@@ -121,6 +147,7 @@ public class Melody {
 		return components.URL ?? undefined("NSURLComponents.URL is nil")
 	}
 
+	/// Initializer
 	public init() {
 		let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
 		session = NSURLSession(configuration: sessionConfiguration)
@@ -141,10 +168,12 @@ public class Melody {
 		return task
 	}
 
+	/// Search for albums
 	public func searchAlbums(term: String, _ completion: ([Album]?, NSError?) -> Void) -> NSURLSessionDataTask {
 		return search(term, .Album, completion)
 	}
 
+	/// Search for tracks
 	public func searchTracks(term: String, _ completion: ([Track]?, NSError?) -> Void) -> NSURLSessionDataTask {
 		return search(term, .Track, completion)
 	}
--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
-.PHONY: test
+.PHONY: all doc test
+
+all: test
+
+doc:
+	jazzy -o doc --podspec Melody.podspec.json
+	@cat doc/undocumented.txt
 
 test:
 	pod lib lint Melody.podspec.json --allow-warnings
